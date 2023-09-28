3212640842a099464828986f95820eaf364d8ebf
luxifa <448933144@qq.com>
2023-09-28 16:28:59 +0800

    通知配置添加查看操作

--- src/components/page/noticeConfig.vue
+++ src/components/page/noticeConfig.vue
@@ -11,7 +11,7 @@
 
       <el-table-column label="操作" style="width: 25%">
         <template slot-scope="scope">
-          <el-button @click="handleClick(scope.row)" type="text" size="medium">查看</el-button>
+          <el-button  type="text" size="medium" @click="viewNoticeConfigClick(scope.row)">查看</el-button>
           <el-button size="medium" type="text" @click="editNoticeConfigClick(scope.row)">编辑</el-button>
         </template>
       </el-table-column>
@@ -31,10 +31,36 @@
         <el-form-item label="间隔时间(分钟)" prop="intervalMinute" :min="0" :max="1440" :label-width="formLabelWidth">
           <el-input v-model="form.intervalMinute" autocomplete="off"></el-input>
         </el-form-item>
+        <el-form-item label="是否启用" style="padding-left: 52px;">
+          <el-switch v-model="form.enable"></el-switch>
+        </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
         <el-button @click="dialogFormVisible = false">取 消</el-button>
         <el-button type="primary" @click="editNoticeConfigCommit">确 定</el-button>
+      </div>
+    </el-dialog>
+    <el-dialog title="查看" :visible.sync="viewDialogFormVisible" width="450px">
+      <el-form :model="form" :rules="rules" :label-position="labelPosition">
+        <el-form-item label="通知类型" :label-width="formLabelWidth">
+          <el-input v-model="form.name" autocomplete="off" :disabled="true"></el-input>
+        </el-form-item>
+        <el-form-item label="用户" :label-width="formLabelWidth">
+          <el-select v-model="form.users" multiple placeholder="请选择用户" disabled>
+
+            <el-option v-for="(item,index) in form.options" :key="index" :label="item" :value="item"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="间隔时间(分钟)" prop="intervalMinute" :min="0" :max="1440" :label-width="formLabelWidth">
+          <el-input v-model="form.intervalMinute" autocomplete="off" :disabled="true"></el-input>
+        </el-form-item>
+        <el-form-item label="是否启用" style="padding-left: 52px;">
+          <el-switch v-model="form.enable" disabled></el-switch>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="viewDialogFormVisible = false">取 消</el-button>
+        <!-- <el-button type="primary" @click="editNoticeConfigCommit">确 定</el-button> -->
       </div>
     </el-dialog>
   </div>
@@ -49,6 +75,71 @@
     methods: {
       handleClick(row) {
         console.log(row);
+      },
+      viewNoticeConfigClick(row){
+        this.getNoticeTypeAll();
+        this.form.name = row.name
+        let data = {
+          params: {
+            name: row.name
+          }
+        }
+        this.form.intervalMinute = row.intervalMinute
+        this.$axios.get("http://127.0.0.1:8080/noticeItem/username", data).then(response => {
+          if (response.data.code == 200) {
+            this.form.users = response.data.data.usernames
+            this.form.enable = response.data.data.enable
+          } else {
+            this.$message({
+              message: response.data.msg,
+              type: "error",
+              showIcon: true
+            })
+          }
+        }).catch(error => {
+          if (error.response) {
+            this.$message({
+              message: error.response.data.msg,
+              type: 'error',
+              showIcon: true
+            });
+          }
+          if (error.message == 'Network Error') {
+            this.$message({
+              message: "无法连接服务器！",
+              type: 'error',
+              showIcon: true
+            });
+          }
+        });
+
+        this.$axios.get("http://127.0.0.1:8080/user/name").then(response => {
+          if (response.data.code == 200) {
+            this.form.options = response.data.data
+          } else {
+            this.$message({
+              message: response.data.msg,
+              type: "error",
+              showIcon: true
+            })
+          }
+        }).catch(error => {
+          if (error.response) {
+            this.$message({
+              message: error.response.data.msg,
+              type: 'error',
+              showIcon: true
+            });
+          }
+          if (error.message == 'Network Error') {
+            this.$message({
+              message: "无法连接服务器！",
+              type: 'error',
+              showIcon: true
+            });
+          }
+        });
+        this.viewDialogFormVisible = true
       },
       getNoticeTypeAll() {
         this.$axios.get("http://127.0.0.1:8080/noticeType").then(response => {
@@ -91,7 +182,8 @@
         this.form.intervalMinute = row.intervalMinute
         this.$axios.get("http://127.0.0.1:8080/noticeItem/username", data).then(response => {
           if (response.data.code == 200) {
-            this.form.users = response.data.data
+            this.form.users = response.data.data.usernames
+            this.form.enable = response.data.data.enable
           } else {
             this.$message({
               message: response.data.msg,
@@ -186,12 +278,14 @@
       return {
         tableData: [],
         dialogFormVisible: false,
+        viewDialogFormVisible: false,
         labelPosition: "right",
         formLabelWidth: '120px',
         form: {
           name: "",
           users: "",
           intervalMinute: "",
+          enable: true,
           options: []
         },
         rules: {
